a9acf8447f399a8df5e254acb8b5209639e3fc4d
shim1998 <98shim@gmail.com>
2021-05-13 21:06:00 +0530

    Figuring out how to get the userlist and discord list together

--- discord_bot.py
+++ discord_bot.py
@@ -5,29 +5,24 @@ class MyClient(discord.Client):
 
     async def on_ready(self):
         self.members=[]
-        print('Logged on as {0}!'.format(self.user))
+        self.channels=[]
+        for guild in self.guilds:
+            for channel in guild.channels:
+                pass
+                # print(channel.id,channel.name)
         for guild in self.guilds:
             data = list(guild.members)
             for x in data:
-                print(x.name,x.id,x.nick)
+                # print(x.name,x.id,x.nick)
                 self.members.append([x.name,x.id,x.nick])
-
+        print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
         channel = message.channel
+        channel.id = 840581432660852746
         if message.content.startswith('hello'):
             await channel.send("Sup {0.author}: {0.content}".format(message))
         if message.content.startswith('tag everyone'):
             for spam in self.members:
                 await channel.send("HI <@"+str(spam[1])+">")
-
-token=""
-with open('discord-token.json','r') as file:
-    for data in file:
-        token+=data
-token = json.loads(token)
-intents = discord.Intents.default()
-intents.members = True
-client = MyClient(intents=intents)
-client.run(token["client-secret"])
